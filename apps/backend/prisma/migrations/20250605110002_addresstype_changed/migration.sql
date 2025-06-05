5a0f21d666d3c6f663d0cdc95eeda9d8e3105244
Jeevan-BYS <jeevan@beyondscale.tech>
2025-06-05 16:30:06 +0530

    added address type in submit verificatin api

---
+++ apps/backend/prisma/migrations/20250605110002_addresstype_changed/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The `addressType` column on the `Verification` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Verification" DROP COLUMN "addressType",
+ADD COLUMN     "addressType" "AddressType";
