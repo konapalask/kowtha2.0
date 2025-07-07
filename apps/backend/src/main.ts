9b9c762568d24aae00cc48f92901070c513fd44f
Jeevan-BYS <jeevan@beyondscale.tech>
2025-07-07 21:45:32 +0530

    deleted org module

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://app.cakowtha.co.in'],
+    origin: ['http://localhost:3000', 'https://app.cakowtha.co.in/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
