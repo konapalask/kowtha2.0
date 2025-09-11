1061f9dde3fa6746debbc911af3107c3d121f1d4
Jeevan-BYS <jeevan@beyondscale.tech>
2025-09-11 11:56:48 +0530

    addded logs

--- apps/backend/src/modules/loan/loan.service.ts
+++ apps/backend/src/modules/loan/loan.service.ts
@@ -107,7 +107,7 @@ export class LoanService {
           loanType: 'Business',
           status: 'Unassigned',
           bankName: data.bankName,
-          loanAmount: data.loanAmount,
+          loanAmount: Number(data.loanAmount),
           applicantName: data.applicantName,
           applicantMobile: data.applicantMobile,
           office: { connect: { id: office.id } },
