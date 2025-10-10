dfcbaa014ad4f495727cf10e47b6b8d809688999
shanmukvinay08 <shanmuk.pakki@beyondscale.tech>
2025-10-10 15:46:18 +0530

    added closed at

--- apps/web/src/pages/loans/index.tsx
+++ apps/web/src/pages/loans/index.tsx
@@ -279,6 +279,13 @@ export default function Loans() {
         render: (date: string) => dayjs(date).format("DD-MM-YYYY"),
         width: 120,
       },
+      {
+        title: "Closed At",
+        dataIndex: "closedAt",
+        key: "closedAt",
+        render: (date: string) => date ? dayjs(date).format("DD-MM-YYYY") : "-",
+        width: 120,
+      },
     ];
 
     if (currentDepartment === 'PD') {
