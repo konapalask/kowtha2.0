8a5ce53b237b733c4e7f7ea07fa8c9748035764e
jayakrishna <jayakrishna@beyondscale.tech>
2025-10-21 17:36:21 +0530

    OTA test change

--- apps/mobile/src/screens/PD.tsx
+++ apps/mobile/src/screens/PD.tsx
@@ -1170,7 +1170,7 @@ const styles = StyleSheet.create({
     borderTopColor: '#ddd',
   },
   submitButton: {
-    backgroundColor: '#3443ff',
+    backgroundColor: '#871db5',
     padding: 16,
     borderRadius: 8,
     alignItems: 'center',
