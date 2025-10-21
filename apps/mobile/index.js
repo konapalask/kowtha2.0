cc972900ae5f26ef49d8f834ac1e397f1775ef0a
shashank-dovient <shashank@dovient.com>
2025-10-21 16:41:09 +0530

    apps issues

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -2,8 +2,37 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
+import CodePush from '@revopush/react-native-code-push';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// Configure CodePush options
+const codePushOptions = {
+  // Check for updates every time the app resumes (enabled in debug for QA)
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+
+  // Install updates immediately (with dialog)
+  installMode: CodePush.InstallMode.IMMEDIATE,
+
+  // Show custom dialog for updates - user gets informed but cannot reject
+  updateDialog: {
+    title: 'Update Available',
+    optionalUpdateMessage:
+      'An update is available. The app will restart after updating.',
+    optionalInstallButtonLabel: 'Update Now',
+    mandatoryUpdateMessage: 'An update is available and must be installed.',
+    mandatoryContinueButtonLabel: 'Restart the application',
+  },
+
+  // Use Android deployment key from strings.xml
+  deploymentKey: Platform.select({
+    android: 'BYneeFfgIcFbUCkZyuEzSzr0ZjUuN1gf9oHafg',
+    ios: null, // iOS not in scope for this project
+  }),
+};
+
+// Wrap App component with CodePush
+const CodePushApp = CodePush(codePushOptions)(App);
+
+AppRegistry.registerComponent(appName, () => CodePushApp);
