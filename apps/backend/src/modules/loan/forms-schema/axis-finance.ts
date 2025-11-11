cbfb9603c3beabb86b252125354942da4023954c
jayakrishna <jayakrishna@beyondscale.tech>
2025-11-11 14:42:49 +0530

    bug fixes

--- apps/backend/src/modules/loan/forms-schema/axis-finance.ts
+++ apps/backend/src/modules/loan/forms-schema/axis-finance.ts
@@ -58,44 +58,53 @@ export const axisFinanceSchema = {
       schema: {
         type: "object",
         properties: {
-          name: {
-            type: "string",
-            title: "Name",
-          },
-          relation: {
-            type: "string",
-            title: "Relation",
-            enum: [
-              "Self",
-              "Spouse",
-              "Son",
-              "Daughter",
-              "Father",
-              "Mother",
-              "Brother",
-              "Sister",
-              "Other",
-            ],
-          },
-          age: {
-            type: "integer",
-            title: "Age",
-          },
-          education: {
-            type: "string",
-            title: "Education",
-            enum: [
-              "Below 10th",
-              "10th pass",
-              "Under graduate",
-              "Graduate",
-              "Post Graduate",
-              "Professional",
-            ],
-          },
-          occupation: {
-            type: "string",
-            title: "Occupation",
+          familyMembers: {
+            type: "array",
+            title: "Family Members",
+            items: {
+              type: "object",
+              properties: {
+                name: {
+                  type: "string",
+                  title: "Name",
+                },
+                relation: {
+                  type: "string",
+                  title: "Relation",
+                  enum: [
+                    "Self",
+                    "Spouse",
+                    "Son",
+                    "Daughter",
+                    "Father",
+                    "Mother",
+                    "Brother",
+                    "Sister",
+                    "Other",
+                  ],
+                },
+                age: {
+                  type: "integer",
+                  title: "Age",
+                },
+                education: {
+                  type: "string",
+                  title: "Education",
+                  enum: [
+                    "Below 10th",
+                    "10th pass",
+                    "Under graduate",
+                    "Graduate",
+                    "Post Graduate",
+                    "Professional",
+                  ],
+                },
+                occupation: {
+                  type: "string",
+                  title: "Occupation",
+                },
+              },
+            },
           },
           noOfDependants: {
             type: "integer",
@@ -107,8 +116,8 @@ export const axisFinanceSchema = {
             enum: ["Good", "Average", "Bad"],
           },
         },
-        required: true,
       },
+      required: true,
     },
     {
       id: "placeOfResidenceOffice",
@@ -367,30 +376,36 @@ export const axisFinanceSchema = {
       id: "otherLiabilitiesIncludingCcLimitsOwnCoApplicants",
       label: "OTHER LIABILITIES INCLUDING CC LIMITS (OWN/CO APPLICANTS)",
       schema: {
-        type: "array",
-        items: {
-          type: "object",
-          properties: {
-            from: {
-              type: "string",
-              title: "From",
-            },
-            natureOfLoan: {
-              type: "string",
-              title: "Nature of Loan",
-              enum: ["Personal Loan", "Home Loan", "Car Loan", "Other"],
-            },
-            amount: {
-              type: "number",
-              title: "O/S Amount",
-            },
-            emi: {
-              type: "number",
-              title: "EMI",
-            },
-            willCloseContinue: {
-              type: "string",
-              title: "Will Close / Continue",
+        type: "object",
+        properties: {
+          otherLiabilities: {
+            type: "array",
+            title: "Other Liabilities",
+            items: {
+              type: "object",
+              properties: {
+                from: {
+                  type: "string",
+                  title: "From",
+                },
+                natureOfLoan: {
+                  type: "string",
+                  title: "Nature of Loan",
+                  enum: ["Personal Loan", "Home Loan", "Car Loan", "Other"],
+                },
+                amount: {
+                  type: "number",
+                  title: "O/S Amount",
+                },
+                emi: {
+                  type: "number",
+                  title: "EMI",
+                },
+                willCloseContinue: {
+                  type: "string",
+                  title: "Will Close / Continue",
+                },
+              },
             },
           },
         },
@@ -470,17 +485,23 @@ export const axisFinanceSchema = {
       id: "tradeReferences",
       label: "Trade References",
       schema: {
-        type: "array",
-        items: {
-          type: "object",
-          properties: {
-            nameOfThePerson: {
-              type: "string",
-              title: "Name of the Person",
-            },
-            contactDetails: {
-              type: "string",
-              title: "Telephone No. / Address for Communication",
+        type: "object",
+        properties: {
+          tradeReferences: {
+            type: "array",
+            title: "Trade References",
+            items: {
+              type: "object",
+              properties: {
+                nameOfThePerson: {
+                  type: "string",
+                  title: "Name of the Person",
+                },
+                contactDetails: {
+                  type: "string",
+                  title: "Telephone No. / Address for Communication",
+                },
+              },
             },
           },
         },
