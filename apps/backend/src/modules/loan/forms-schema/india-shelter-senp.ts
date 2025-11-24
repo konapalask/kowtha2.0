d56efed337f83bb49f928769a267d80421debec3
jayakrishna <jayakrishna@beyondscale.tech>
2025-11-24 15:41:45 +0530

    bug fixes

--- apps/backend/src/modules/loan/forms-schema/india-shelter-senp.ts
+++ apps/backend/src/modules/loan/forms-schema/india-shelter-senp.ts
@@ -39,6 +39,7 @@ export const indiaShelterSenpSchema = {
           applicantName: {
             type: "string",
             title: "Name of the Applicant",
+            readOnly: true,
           },
           maritalStatus: {
             type: "string",
@@ -49,14 +50,12 @@ export const indiaShelterSenpSchema = {
             type: "string",
             title: "Educational Qualification",
             enum: [
-              "Below 10",
-              "10th Pass",
-              "12th Pass",
-              "Diploma",
-              "ITI Certification",
+              "Below 10th",
+              "10th pass",
+              "12th pass",
+              "Diploma/ITI certification",
               "Graduate",
-              "PG",
-              "Professional Certification",
+              "PG/Professional Certification",
             ],
           },
           category: {
@@ -511,6 +510,7 @@ export const indiaShelterSenpSchema = {
           businessName: {
             type: "string",
             title: "Name of Current Business Firm",
+            readOnly: true,
           },
           businessFirmType: {
             type: "string",
@@ -532,6 +532,7 @@ export const indiaShelterSenpSchema = {
           commencementDate: {
             type: "string",
             title: "Date of commencement of Business",
+            format: "date",
           },
           placeOfIncorporation: {
             type: "string",
@@ -548,6 +549,7 @@ export const indiaShelterSenpSchema = {
               widget: "textarea",
               rows: 2,
             },
+            readOnly: true,
           },
           totalWorkExperienceYears: {
             type: "number",
