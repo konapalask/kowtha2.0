94058fe672c693b45d27ac74e8a45e897feeee08
jayakrishna <jayakrishna@beyondscale.tech>
2025-12-26 14:00:58 +0530

    bug fix

--- apps/backend/src/modules/loan/forms-schema/jana-salaried.ts
+++ apps/backend/src/modules/loan/forms-schema/jana-salaried.ts
@@ -208,13 +208,13 @@ export const janaSalariedSchema = {
     },
     {
       id: "networthDetails",
-      label: "Networth Details",
+      label: "Net Worth (Car / Property / Investments etc.)",
       schema: {
         type: "object",
         properties: {
           netWorth: {
             type: "array",
-            title: "Net Worth (Car / Property / Investments etc.)",
+            title: "Net Worth",
             items: {
               type: "object",
               properties: {
