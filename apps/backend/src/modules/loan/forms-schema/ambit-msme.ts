25a1d14016dd5fe8e3297a5f640ad3c189e8e2b0
Sandeep Reddy <sandeepbhumireddy9@gmail.com>
2026-02-23 12:39:55 +0530

    Few enhancements in pdf generation

--- apps/backend/src/modules/loan/forms-schema/ambit-msme.ts
+++ apps/backend/src/modules/loan/forms-schema/ambit-msme.ts
@@ -589,7 +589,7 @@ export const ambitMsmeSchema = {
         properties: {
           endUse: {
             type: "string",
-            title: "End Use of Loan Purppose",
+            title: "End Use of Loan Purpose",
             ui: {
               widget: "textarea",
               rows: 3,
